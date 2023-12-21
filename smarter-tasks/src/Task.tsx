70d5826fedb7bdcdced8227915dded17ec94d4f7
Aayus <godlord@AayushPathak>
2023-12-21 12:33:21 +0530

    L3 fix netlify 2

--- smarter-tasks/src/Task.tsx
+++ smarter-tasks/src/Task.tsx
@@ -9,9 +9,9 @@ class Task extends React.Component<TaskProp> {
   render() {
     return (
       <div className="TaskItem shadow-md border border-slate-100">
-        <h2 className="TaskItem text-base font-bold my-1">Title: {this.props.todoTitle}</h2>
-        <p className="TaskItem text-sm text-slate-500">Description: {this.props.todoDueDate}</p>
-        <p className="TaskItem text-sm text-slate-500">DueDate: {this.props.todoDescription}</p>
+        <h2 className="text-base font-bold my-1">Title: {this.props.todoTitle}</h2>
+        <p className="text-sm text-slate-500">Description: {this.props.todoDueDate}</p>
+        <p className="text-sm text-slate-500">DueDate: {this.props.todoDescription}</p>
       </div>
     );
   }
