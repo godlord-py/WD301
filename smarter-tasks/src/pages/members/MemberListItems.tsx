01dee7a4f5f8c3cf77d266c8d7bd2af4a40e68f0
Aayus <godlord@AayushPathak>
2024-01-01 16:20:06 +0530

    L7 fix try 4

--- smarter-tasks/src/pages/members/MemberListItems.tsx
+++ smarter-tasks/src/pages/members/MemberListItems.tsx
@@ -48,7 +48,7 @@ export default function MemberListItems() {
   return (
     <>
       {members.map((member: Member) => (
-        <div key={`${member.name}_${member.email}`} className="block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+        <div key={`${member.name}_${member.email}`} className=" member block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
           <h5 className="mb-2 text-xl font-medium tracking-tight text-gray-900 dark:text-white">{member.name}</h5>
           <h5 className="mb-2 text-xl font-medium tracking-tight text-gray-900 dark:text-white">{member.email}</h5>
           <button
