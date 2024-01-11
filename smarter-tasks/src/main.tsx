51409e92c04b756e2c4e1e8a30841f4d5a25377d
Aayus <godlord@AayushPathak>
2024-01-11 17:52:19 +0530

    Level 9

--- smarter-tasks/src/main.tsx
+++ smarter-tasks/src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { ThemeProvider } from "./context/theme";
+import { ThemeProvider } from "./context/theme.tsx";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <ThemeProvider>
     <App />
